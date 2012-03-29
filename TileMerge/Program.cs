196919c3447f3817fec2c4a87871e7e08ec9e1f9
Maarten De Wilde <maarten.dewilde@gmail.com>
2012-03-29 19:07:21 +0200

    Add grouping and coloring of start of animations

--- TileMerge/Program.cs
+++ TileMerge/Program.cs
@@ -10,6 +10,8 @@ namespace TileMerge
 {
     class Program
     {
+        const int animationImageSufficSize = 7;
+
         static void Main(string[] args)
         {
             var images = Directory.GetFiles(".", "*.png").OrderBy(s => s).ToList();
@@ -21,11 +23,11 @@ namespace TileMerge
             Bitmap firstBitmapFileAsSizeReference = (Bitmap)Bitmap.FromFile(images.First());
             var size = firstBitmapFileAsSizeReference.Width;
             
-            Bitmap tileSetImage = new Bitmap(size * numberOfTilesPerSize, size * numberOfTilesPerSize, PixelFormat.Format32bppArgb);   
+            Bitmap tileSetImage = new Bitmap(size * numberOfTilesPerSize, size * numberOfTilesPerSize, PixelFormat.Format32bppPArgb);   
             Graphics tileSetGraphicsObject = Graphics.FromImage(tileSetImage);
             tileSetImage.SetResolution(firstBitmapFileAsSizeReference.HorizontalResolution, firstBitmapFileAsSizeReference.VerticalResolution);
 
-            Bitmap infoImage = new Bitmap(size * numberOfTilesPerSize, size * numberOfTilesPerSize, PixelFormat.Format32bppArgb);
+            Bitmap infoImage = new Bitmap(size * numberOfTilesPerSize, size * numberOfTilesPerSize, PixelFormat.Format32bppPArgb);
             Graphics infoGraphicsObject = Graphics.FromImage(infoImage);      
             infoImage.SetResolution(firstBitmapFileAsSizeReference.HorizontalResolution, firstBitmapFileAsSizeReference.VerticalResolution);
 
@@ -33,13 +35,18 @@ namespace TileMerge
 
             Font drawFont = new Font("Arial", 16);
             SolidBrush drawBrush = new SolidBrush(Color.Black);
-           
+            SolidBrush newBrush = new SolidBrush(Color.Red);
+
+            var firstOfGroup = images.GroupBy(img => img.Substring(0, img.Length - animationImageSufficSize)).ToDictionary(t => t, t => t.ToList()).Select(t => t.Value.First()).ToList();
+
             for (int index = 0; index < images.Count; index++)
             {
+                bool isFirstOfGroup = firstOfGroup.Contains(images[index]);
+
                 var imageBitMap = (Bitmap)Bitmap.FromFile(images[index]);
                 tileSetGraphicsObject.DrawImage(imageBitMap, offsetOfCurrentImage);
                 infoGraphicsObject.DrawImage(imageBitMap, offsetOfCurrentImage);
-                infoGraphicsObject.DrawString(index.ToString(), drawFont, drawBrush, new PointF(offsetOfCurrentImage.X + 5, offsetOfCurrentImage.Y + 5));
+                infoGraphicsObject.DrawString(index.ToString(),drawFont, isFirstOfGroup ? newBrush :  drawBrush, new PointF(offsetOfCurrentImage.X + 5, offsetOfCurrentImage.Y + 5));
                 imageBitMap.Dispose();
 
                 if (offsetOfCurrentImage.X + size < size * numberOfTilesPerSize)
